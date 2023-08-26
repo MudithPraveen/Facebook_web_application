41fd126d4ab239050feeadd1fedd81f4a5dd85f1
Mudith praveen <mudithpravin2@gmail.com>
2023-08-26 13:14:16 +0530

    icon adding

--- facebook/src/components/Navigation/NavBar.jsx
+++ facebook/src/components/Navigation/NavBar.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import SearchIcon from '@mui/icons-material/Search';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import StoreRoundedIcon from '@mui/icons-material/StoreRounded';
+import PeopleAltRoundedIcon from '@mui/icons-material/PeopleAltRounded';
+import VideogameAssetRoundedIcon from '@mui/icons-material/VideogameAssetRounded';
 
 export default function NavBar() {
   return (
@@ -11,9 +16,18 @@ export default function NavBar() {
             </div>
             <div className="searchBarBox">
                 <SearchIcon className='serchIcon'/>
+                <input type="text" placeholder='Search Facebook'className="searchInput" />
             </div>
         </div>
-        <div className="navBarCenter"></div>
+        <div className="navBarCenter">
+            <div className="pages">
+                <HomeRoundedIcon/>
+                <VideoLibraryIcon/>
+                <StoreRoundedIcon/>
+                <PeopleAltRoundedIcon/>
+                <VideogameAssetRoundedIcon/>
+            </div>
+        </div>
         <div className="navBarRight"></div>
     </div>
   )
